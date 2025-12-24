1cf46b0ce3b52a652da649e2a3c44b09092b8cc9
anhpi1 <115565002+anhpi1@users.noreply.github.com>
2025-12-24 13:56:43 +0700

    none

--- Hai_Anh_main.c
+++ Hai_Anh_main.c
@@ -33,3 +33,13 @@ int main() {
     } 
     return 0; 
 }
+
+// dựa vào hàm main hãu viết sever.c và sever.h để khởi động sever 
+// lưu ý
+// DWORD WINAPI ThreadFunc3(LPVOID lpParam)
+// {
+// HOOK_replay_events("log/mouse_log0.csv", "log/keyboard_log0.csv", 2);
+// return 0;
+// }
+// cái này truyền vào có tham số tham số cuối dùng là mode 0 
+// là chỉ phát chuột 1 là chỉ phát bàn phím 2 là phát cả hai viết client.c có menu để test
